57ff46e6cc5b1a0739a99c972704dbe2780b0d44
Avicena Fathoni Fawwaz <tracerbuzzstudy@gmail.com>
2026-09-16 22:48:53 +0700

    Refactor to Class

--- index.js
+++ index.js
@@ -3,19 +3,43 @@ let tanggalHariIni = new Date();
 console.log(`\n Selamat datang di ${namaToko}, tanggal hari ini:
     ${tanggalHariIni}\n`);
 
-const daftarBarang = [   { id: 1, nama: "Pedal Anti-slip", stok: 24, harga: 45000},
-                    { id: 2, nama: "Grease Anti-garing (125gr)", stok: 37, harga: 13000},
-                    { id: 3, nama: "Mud Guard", stok: 40, harga: 20000},
-                    { id: 4, nama: "Saddle Aero", stok: 17, harga: 65000},
-                    { id: 5, nama: "Saddle Empuk++", stok: 9, harga: 40000},
-                    { id: 6, nama: "Saddle Aja", stok: 44, harga: 20000},
-                    { id: 7, nama: "WD-40", stok: 57, harga: 21000},
-                    { id: 8, nama: "Chainguard", stok: 14, harga: 30000},
-                    { id: 9, nama: "Brake Liquid", stok: 6, harga: 15000},
-];
+class StokManager {
+    constructor() {
+        this.daftarBarang = [];
+    }
 
-    console.log(`                       --=== STOK PEDALWORKS ===--`)
-daftarBarang.forEach((b) => {
-    nilaiStok = b.stok * b.harga;
-    console.log(`ID: ${b.id} Nama: ${b.nama} Stok: ${b.stok} Harga: ${b.harga} Total nilai stok: ${nilaiStok}`)
-});
+    tambah(nama, stok, harga) {
+        const idBaru = this.daftarBarang.length + 1;
+        this.daftarBarang.push({ id: idBaru, nama, stok, harga });
+    }
+
+    tampilkan() {
+        console.log(`                       --=== STOK PEDALWORKS ===--`)
+        this.daftarBarang.forEach(b => {
+            nilaiStok = b.stok * b.harga;
+            console.log(`${b.id} - ${b.nama} - Stok: ${b.stok} - Rp${b.harga} Nilai Stok: ${nilaiStok}`)
+        });
+    }
+
+    barangMasuk(id, jumlah) {
+        const barang = this.daftarBarang.find((b) => b.id === id);
+        if (barang) barang.stok += jumlah;
+    }
+
+    barangKeluar(id, jumlah) {
+        const barang = this.daftarBarang.find((b) => b.id === id);
+        if (barang && barang.stok >= jumlah) {
+            barang.stok -= jumlah;
+        } else {
+            console.log("Stok tidak mencukupi!");
+        }
+    }
+
+    hapus(id) {
+        this.daftarBarang = this.daftarBarang.filter((b) => b.id !== id);
+    }
+
+    cekStokMenipis(batas = 10) {
+        return this.daftarBarang.filter((b) => b.stok <= batas);
+    }
+}
